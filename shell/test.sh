16373e62820758f7cc6c4ff3e8f552ef9c3dbae2
showa-93 <shumai.1215.2018@gmail.com>
2022-09-04 16:52:14 +0900

    tesr.shに誤差ジャッジのオプションを追加

--- shell/test.sh
+++ shell/test.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 base_dir=./
 
-if [ $# == 1 ]; then
-  base_dir=$1
-fi
+while (( $# > 0 ))
+do
+  case $1 in
+    -b) # テストの対象となるディレクトリをルートディレクトリから変更する
+      if [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+        echo "'option' requires an argument." 1>&2
+        exit 1
+      else
+        base_dir="$2"
+        shift
+      fi
+      ;;
+    -e) # 許容する誤差を指定する
+      if [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+        echo "'option' requires an argument." 1>&2
+        exit 1
+      else
+        measurement_error=" -e ${2}"
+        shift
+      fi
+      ;;
+    -*)
+      echo "Illegal option -- '$(echo $1 | sed 's/^-*//')'." 1>&2
+      exit 1.
+  esac
+  shift
+done
 
-(cd $base_dir; oj t -D -c "go run main.go" -d testdata -f "%s/%e")
+(cd $base_dir; oj t -D -c "go run main.go" -d testdata -f "%s/%e"${measurement_error})
