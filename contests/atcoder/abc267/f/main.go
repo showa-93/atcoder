40502ac8cde4ec8995ee7a4605ac54f72feb95b8
showa-93 <shumai.1215.2018@gmail.com>
2022-09-11 03:17:49 +0900

    atcoder abc267 f

--- contests/atcoder/abc267/f/main.go
+++ contests/atcoder/abc267/f/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"container/list"
 	"io"
 	"os"
 	"strconv"
@@ -24,57 +25,90 @@ func solve(in io.Reader, out io.Writer) {
 	defer w.Flush()
 
 	n := r.ReadInt()
-	edgeList := make([][]int, n-1)
+	graph := make([][]int, n)
 	for i := 0; i < n-1; i++ {
-		edgeList[i] = r.ReadIntLine(2)
+		u, v := r.ReadInt(), r.ReadInt()
+		u -= 1
+		v -= 1
+		graph[u] = append(graph[u], v)
+		graph[v] = append(graph[v], u)
 	}
 
-	// わーシャルフロイドで解こうとするがおおきすぎてTLE
-	distanceList := make([][]int, n)
-	for i := 0; i < n; i++ {
-		distanceList[i] = make([]int, n)
-		for j := 0; j < n; j++ {
-			if i != j {
-				distanceList[i][j] = MaxInt
-			}
-		}
-	}
-
-	for k := 0; k < len(edgeList); k++ {
-		distanceList[edgeList[k][0]-1][edgeList[k][1]-1] = 1
-	}
-
-	for k := 0; k < n; k++ {
-		for i := 0; i < n; i++ {
-			// 経由点への経路が存在しない場合スキップする
-			if distanceList[i][k] == MaxInt {
-				continue
-			}
-			for j := 0; j < n; j++ {
-				// 経由点への経路が存在しない場合スキップする
-				if distanceList[k][j] == MaxInt {
-					continue
-				}
-
-				// 最短の方で「始点→終点」への距離を更新する
-				distanceList[i][j] = Min(distanceList[i][j], distanceList[i][k]+distanceList[k][j])
-				distanceList[j][i] = distanceList[i][j]
-			}
-		}
-	}
-
+	// クエリを頂点ごとに集計しとく
 	q := r.ReadInt()
+	query := make([][][2]int, n)
 	for i := 0; i < q; i++ {
-		query := r.ReadIntLine(2)
-		d := -1
-		for i, v := range distanceList[query[0]-1] {
-			if v == query[1] {
-				d = i + 1
-				break
+		u := r.ReadInt() - 1
+		query[u] = append(query[u], [2]int{i, r.ReadInt()})
+	}
+
+	right := bfs(n, 0, graph)
+	left := bfs(n, right, graph)
+
+	ans := make([]int, q)
+	for i := 0; i < q; i++ {
+		ans[i] = -1
+	}
+
+	// 木の直系のどちらか端から数えた時の深さを調べる
+	// 対象の頂点から祖先にかえったときのパス上の頂点を調べるので
+	// 深さ優先探索
+	var dfs func(u int, p int)
+	for _, root := range []int{left, right} {
+		path := make([]int, 0)
+		dfs = func(u int, p int) {
+			// 対象の頂点にクエリが存在する場合、
+			// pathの配列の深さから対象の頂点を計算する
+			for _, qq := range query[u] {
+				// クエリの深さよりパスが長ければパス上に存在
+				if qq[1] <= len(path) {
+					// 頂点から順にpathに詰まるので、
+					// 調べたい深さdの位置は長さ-d
+					ans[qq[0]] = path[len(path)-qq[1]] + 1 // この+1は0始まりにするためにひいてるから帳尻合わせ
+				}
+			}
+
+			// 子で続行するので、pathにappend
+			path = append(path, u)
+			for _, v := range graph[u] {
+				// 親と一致しないときだけ
+				if v != p {
+					dfs(v, u)
+				}
+			}
+			// おわったので、取り出す
+			path = path[:len(path)-1]
+		}
+		dfs(root, -1)
+	}
+
+	for _, a := range ans {
+		w.WriteInt(a)
+	}
+}
+
+func bfs(n, root int, graph [][]int) int {
+	var (
+		que           = list.New()
+		d             = make([]int, n)
+		vmax, max int = root, 0
+	)
+	que.PushFront(root)
+	for que.Len() != 0 {
+		u := que.Remove(que.Back()).(int)
+		for _, v := range graph[u] {
+			if v != root && d[v] == 0 {
+				d[v] = d[u] + 1
+				if max < d[v] {
+					max = d[v]
+					vmax = v
+				}
+				que.PushFront(v)
 			}
 		}
-		w.WriteInt(d)
 	}
+
+	return vmax
 }
 
 type reader struct {
