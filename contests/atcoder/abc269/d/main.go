b29e772e01ad5a5a1f677629ad3d29c9d4da8018
showa-93 <shumai.1215.2018@gmail.com>
2022-09-17 23:31:31 +0900

    atcoder abc269 e 別解を追加

--- contests/atcoder/abc269/d/main.go
+++ contests/atcoder/abc269/d/main.go
@@ -16,7 +16,8 @@ const (
 )
 
 func main() {
-	solve(os.Stdin, os.Stdout)
+	// solve(os.Stdin, os.Stdout)
+	solve2(os.Stdin, os.Stdout)
 }
 
 func solve(in io.Reader, out io.Writer) {
@@ -60,6 +61,102 @@ func solve(in io.Reader, out io.Writer) {
 	}
 
 	w.WriteInt(count)
+}
+
+// 各頂点をグラフにみなして、UnionFindで素な集合の数を数える
+func solve2(in io.Reader, out io.Writer) {
+	r := NewReader(in)
+	w := NewWriter(out)
+	defer w.Flush()
+	n := r.ReadInt()
+
+	pairs := make([][2]int, n)
+	mp := make(map[[2]int]int) // 頂点の存在チェック用
+	for i := 0; i < n; i++ {
+		pairs[i] = [2]int{r.ReadInt(), r.ReadInt()}
+		mp[pairs[i]] = i
+	}
+
+	dsf := NewDejointSetsForests(n)
+	check := func(n int, p [2]int) {
+		if i, ok := mp[p]; ok {
+			dsf.Unite(n, i)
+		}
+	}
+
+	// 各ペアで片側の隣り合うペアが存在するかチェックし、
+	// 存在する場合、そのペアをdejoint setsに追加する
+	// 片側の座標で問題ないのは、もし隣り合うペアがもう片側にあった場合、
+	// もう片側にあるペアのほうで検出できるため片側で問題ない
+	for i, pair := range pairs {
+		x, y := pair[0], pair[1]
+		check(i, [2]int{x + 1, y})
+		check(i, [2]int{x, y + 1})
+		check(i, [2]int{x + 1, y + 1})
+	}
+
+	count := 0
+	for i := 0; i < n; i++ {
+		// dsfの頂点になっているペアの数を数えることで対象を求める
+		if dsf.FindSet(i) == i {
+			count++
+		}
+	}
+	w.WriteInt(count)
+}
+
+// 素な集合
+type DejointSetsForests struct {
+	parent []int
+	rank   []int
+}
+
+func NewDejointSetsForests(n int) *DejointSetsForests {
+	dsf := &DejointSetsForests{
+		parent: make([]int, n),
+		rank:   make([]int, n),
+	}
+
+	for i := 1; i < n; i++ {
+		dsf.MakeSet(i)
+	}
+
+	return dsf
+}
+
+func (dsf *DejointSetsForests) MakeSet(x int) {
+	dsf.rank[x] = 0
+	dsf.parent[x] = x
+}
+
+// ルートを返す
+func (dsf *DejointSetsForests) FindSet(x int) int {
+	// 対象がルート出ない場合、親の探索をおこない、経路圧縮をおこなう
+	// ルートを直接参照させる
+	if x != dsf.parent[x] {
+		dsf.parent[x] = dsf.FindSet(dsf.parent[x])
+	}
+	return dsf.parent[x]
+}
+
+func (dsf *DejointSetsForests) Same(x, y int) bool {
+	return dsf.FindSet(x) == dsf.FindSet(y)
+}
+
+func (dsf *DejointSetsForests) Unite(x, y int) {
+	// ルートで結合するほうの比較をおこなう
+	rootX, rootY := dsf.FindSet(x), dsf.FindSet(y)
+	// 高さが高い方の木に低い木をつなげる
+	if dsf.rank[rootX] > dsf.rank[rootY] {
+		// 低い方のルートを変更する
+		dsf.parent[rootY] = rootX
+	} else {
+		dsf.parent[rootX] = rootY
+	}
+	if dsf.rank[rootX] == dsf.rank[rootY] {
+		// 同じ高さの場合、つなげると１つ高くなるので、加算する
+		dsf.rank[rootY]++
+	}
 }
 
 type reader struct {
