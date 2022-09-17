9c1c1289a9c5ad8bd86e802fc9e99d679fe1bee8
showa-93 <shumai.1215.2018@gmail.com>
2022-09-17 21:42:28 +0900

    atcoder abc269 c

---
+++ contests/atcoder/abc269/c/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+const BufferSize int = 1e9
+
+const (
+	MinInt = -1 << (64 - 1)
+	MaxInt = 1<<(64-1) - 1
+)
+
+func main() {
+	solve(os.Stdin, os.Stdout)
+}
+
+func solve(in io.Reader, out io.Writer) {
+	r := NewReader(in)
+	w := NewWriter(out)
+	defer w.Flush()
+	n := r.ReadInt()
+
+	digits := strconv.FormatInt(int64(n), 2)
+	result := make([]int, 0, 1024)
+
+	for i, v := range digits {
+		if v == '1' {
+			x := int(math.Pow(2, float64(len(digits)-(i+1))))
+			for _, r := range result {
+				result = append(result, x+r)
+			}
+			result = append(result, x)
+		}
+	}
+
+	result = append(result, 0)
+	sort.Slice(result, func(i, j int) bool { return result[i] < result[j] })
+	for _, v := range result {
+		w.WriteInt(v)
+	}
+}
+
+type reader struct {
+	s *bufio.Scanner
+}
+
+func NewReader(r io.Reader) *reader {
+	s := bufio.NewScanner(r)
+	s.Buffer(make([]byte, BufferSize), BufferSize)
+	s.Split(bufio.ScanWords)
+	return &reader{
+		s: s,
+	}
+}
+
+func (r *reader) Read() string {
+	r.s.Scan()
+	return r.s.Text()
+}
+
+func (r *reader) ReadInt() int {
+	r.s.Scan()
+	num, _ := strconv.Atoi(r.s.Text())
+
+	return num
+}
+
+func (r *reader) ReadLine(n int) []string {
+	line := make([]string, n)
+	for i := 0; i < n; i++ {
+		line[i] = r.Read()
+	}
+	return line
+}
+
+func (r *reader) ReadIntLine(n int) []int {
+	line := make([]int, n)
+	for i := 0; i < n; i++ {
+		line[i] = r.ReadInt()
+	}
+	return line
+}
+
+type writer struct {
+	w *bufio.Writer
+}
+
+func NewWriter(w io.Writer) *writer {
+	return &writer{
+		w: bufio.NewWriter(w),
+	}
+}
+
+func (w *writer) Flush() error {
+	return w.w.Flush()
+}
+
+func (w *writer) WriteString(s string) {
+	w.w.WriteString(s)
+	w.w.WriteRune('\n')
+}
+
+func (w *writer) WriteInt(v int) {
+	w.w.WriteString(strconv.Itoa(v))
+	w.w.WriteRune('\n')
+}
+
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
+}
