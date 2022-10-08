267d9cf1c3f2ca7db1101cce6fa939d7be9c5540
showa-93 <shumai.1215.2018@gmail.com>
2022-10-08 21:19:44 +0900

    atcoder abc272 b

---
+++ contests/atcoder/abc272/b/main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+)
+
+const BufferSize int = 1e9
+
+const (
+	MinInt = -1 << (64 - 1)
+	MaxInt = 1<<(64-1) - 1
+)
+
+func main() {
+	solve(os.Stdin, os.Stdout)
+}
+
+func solve(in io.Reader, out io.Writer) {
+	r := NewReader(in)
+	w := NewWriter(out)
+	defer w.Flush()
+	n, m := r.ReadInt(), r.ReadInt()
+	list := make([]map[int]int, n)
+	for i := 0; i < n; i++ {
+		list[i] = make(map[int]int)
+	}
+
+	for i := 0; i < m; i++ {
+		k := r.ReadInt()
+		l := r.ReadIntLine(k)
+		sort.Slice(l, func(i, j int) bool { return l[i] > l[j] })
+
+		for j := 1; j < k; j++ {
+			for k := 0; k < len(l[:j]); k++ {
+				list[l[j]-1][l[k]-1] = 1
+			}
+		}
+	}
+
+	for i, x := range list {
+		if len(x) != n-(i+1) {
+			w.WriteString("No")
+			return
+		}
+	}
+	w.WriteString("Yes")
+}
+
+type reader struct {
+	s *bufio.Scanner
+}
+
+func NewReader(r io.Reader) *reader {
+	s := bufio.NewScanner(r)
+	s.Buffer(make([]byte, BufferSize), BufferSize)
+	s.Split(bufio.ScanWords)
+	return &reader{
+		s: s,
+	}
+}
+
+func (r *reader) Read() string {
+	r.s.Scan()
+	return r.s.Text()
+}
+
+func (r *reader) ReadInt() int {
+	r.s.Scan()
+	num, _ := strconv.Atoi(r.s.Text())
+
+	return num
+}
+
+func (r *reader) ReadLine(n int) []string {
+	line := make([]string, n)
+	for i := 0; i < n; i++ {
+		line[i] = r.Read()
+	}
+	return line
+}
+
+func (r *reader) ReadIntLine(n int) []int {
+	line := make([]int, n)
+	for i := 0; i < n; i++ {
+		line[i] = r.ReadInt()
+	}
+	return line
+}
+
+type writer struct {
+	w *bufio.Writer
+}
+
+func NewWriter(w io.Writer) *writer {
+	return &writer{
+		w: bufio.NewWriter(w),
+	}
+}
+
+func (w *writer) Flush() error {
+	return w.w.Flush()
+}
+
+func (w *writer) WriteString(s string) {
+	w.w.WriteString(s)
+	w.w.WriteRune('\n')
+}
+
+func (w *writer) WriteInt(v int) {
+	w.w.WriteString(strconv.Itoa(v))
+	w.w.WriteRune('\n')
+}
+
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
+}
+
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+
+	return y
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+
+	return x
+}
