149642ea35ca1f7d72abdfc10a66a876fd647639
showa-93 <shumai.1215.2018@gmail.com>
2022-10-30 02:23:30 +0900

    atcoder abc275 f

---
+++ contests/atcoder/abc275/f/main.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"strconv"
+)
+
+const BufferSize int = 1e9
+
+const (
+	MinInt = -1 << (64 - 1)
+	MaxInt = 1<<(64-1) - 1
+)
+
+func main() {
+	solve(os.Stdin, os.Stdout)
+}
+
+func solve(in io.Reader, out io.Writer) {
+	r := NewReader(in)
+	w := NewWriter(out)
+	defer w.Flush()
+	n, m := r.ReadInt(), r.ReadInt()
+	a := r.ReadIntLine(n)
+	prev := new2dInt(m+1, 2, MaxInt)
+	next := new2dInt(m+1, 2, MaxInt)
+
+	prev[0][0] = 0
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < m+1; j++ {
+			for k := 0; k < 2; k++ {
+				now := prev[j][k]
+				if now != MaxInt {
+					{
+						nj := j + a[i]
+						if nj <= m {
+							next[nj][0] = Min(next[nj][0], now)
+						}
+					}
+					{
+						cost := 0
+						if k == 0 {
+							cost = 1
+						}
+						next[j][1] = Min(next[j][1], now+cost)
+					}
+				}
+			}
+		}
+		prev, next = next, prev
+		reset2dInt(next, MaxInt)
+	}
+
+	for i := 1; i < m+1; i++ {
+		ans := Min(prev[i][0], prev[i][1])
+		if ans == MaxInt {
+			ans = -1
+		}
+		w.WriteInt(ans)
+	}
+}
+
+type reader struct {
+	s *bufio.Scanner
+}
+
+func NewReader(r io.Reader) *reader {
+	s := bufio.NewScanner(r)
+	s.Buffer(make([]byte, BufferSize), BufferSize)
+	s.Split(bufio.ScanWords)
+	return &reader{
+		s: s,
+	}
+}
+
+func (r *reader) Read() string {
+	r.s.Scan()
+	return r.s.Text()
+}
+
+func (r *reader) ReadInt() int {
+	r.s.Scan()
+	num, _ := strconv.Atoi(r.s.Text())
+
+	return num
+}
+
+func (r *reader) ReadLine(n int) []string {
+	line := make([]string, n)
+	for i := 0; i < n; i++ {
+		line[i] = r.Read()
+	}
+	return line
+}
+
+func (r *reader) ReadIntLine(n int) []int {
+	line := make([]int, n)
+	for i := 0; i < n; i++ {
+		line[i] = r.ReadInt()
+	}
+	return line
+}
+
+type writer struct {
+	w *bufio.Writer
+}
+
+func NewWriter(w io.Writer) *writer {
+	return &writer{
+		w: bufio.NewWriter(w),
+	}
+}
+
+func (w *writer) Flush() error {
+	return w.w.Flush()
+}
+
+func (w *writer) WriteString(s string) {
+	w.w.WriteString(s)
+	w.w.WriteRune('\n')
+}
+
+func (w *writer) WriteInt(v int) {
+	w.w.WriteString(strconv.Itoa(v))
+	w.w.WriteRune('\n')
+}
+
+func new2dInt(m, n, v int) [][]int {
+	table := make([][]int, m)
+	for i := 0; i < m; i++ {
+		table[i] = make([]int, n)
+		for j := 0; j < n; j++ {
+			table[i][j] = v
+		}
+	}
+
+	return table
+}
+
+func reset2dInt(table [][]int, v int) {
+	for i := 0; i < len(table); i++ {
+		for j := 0; j < len(table[i]); j++ {
+			table[i][j] = v
+		}
+	}
+}
+
+func copy2dInt(dst, src [][]int) {
+	for i := 0; i < len(src); i++ {
+		copy(dst[i], src[i])
+	}
+}
+
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
+}
+
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+
+	return y
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+
+	return x
+}
