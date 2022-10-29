abdd08c16ddb627dca9dce202d33ccbabf7aedd2
showa-93 <shumai.1215.2018@gmail.com>
2022-10-29 21:33:01 +0900

    atcoder abc275 d

---
+++ contests/atcoder/abc275/d/main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"strconv"
+)
+
+const BufferSize int = 1e9
+
+const (
+	MinInt = -1 << (64 - 1)
+	MaxInt = 1<<(64-1) - 1
+)
+
+func main() {
+	solve(os.Stdin, os.Stdout)
+}
+
+func solve(in io.Reader, out io.Writer) {
+	r := NewReader(in)
+	w := NewWriter(out)
+	defer w.Flush()
+	n := r.ReadInt()
+
+	memo := make(map[int]int)
+	memo[0] = 1
+	var calc func(int) int
+	calc = func(i int) int {
+		if v, ok := memo[i]; ok {
+			return v
+		}
+		memo[i] = calc(i/2) + calc(i/3)
+		return memo[i]
+	}
+
+	w.WriteInt(calc(n))
+}
+
+type reader struct {
+	s *bufio.Scanner
+}
+
+func NewReader(r io.Reader) *reader {
+	s := bufio.NewScanner(r)
+	s.Buffer(make([]byte, BufferSize), BufferSize)
+	s.Split(bufio.ScanWords)
+	return &reader{
+		s: s,
+	}
+}
+
+func (r *reader) Read() string {
+	r.s.Scan()
+	return r.s.Text()
+}
+
+func (r *reader) ReadInt() int {
+	r.s.Scan()
+	num, _ := strconv.Atoi(r.s.Text())
+
+	return num
+}
+
+func (r *reader) ReadLine(n int) []string {
+	line := make([]string, n)
+	for i := 0; i < n; i++ {
+		line[i] = r.Read()
+	}
+	return line
+}
+
+func (r *reader) ReadIntLine(n int) []int {
+	line := make([]int, n)
+	for i := 0; i < n; i++ {
+		line[i] = r.ReadInt()
+	}
+	return line
+}
+
+type writer struct {
+	w *bufio.Writer
+}
+
+func NewWriter(w io.Writer) *writer {
+	return &writer{
+		w: bufio.NewWriter(w),
+	}
+}
+
+func (w *writer) Flush() error {
+	return w.w.Flush()
+}
+
+func (w *writer) WriteString(s string) {
+	w.w.WriteString(s)
+	w.w.WriteRune('\n')
+}
+
+func (w *writer) WriteInt(v int) {
+	w.w.WriteString(strconv.Itoa(v))
+	w.w.WriteRune('\n')
+}
+
+func new2dInt(m, n int) [][]int {
+	table := make([][]int, m)
+	for i := 0; i < m; i++ {
+		table[i] = make([]int, n)
+	}
+
+	return table
+}
+
+func reset2dInt(table [][]int) {
+	for i := 0; i < len(table); i++ {
+		for j := 0; j < len(table[i]); j++ {
+			table[i][j] = 0
+		}
+	}
+}
+
+func copy2dInt(dst, src [][]int) {
+	for i := 0; i < len(src); i++ {
+		copy(dst[i], src[i])
+	}
+}
+
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
+}
+
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+
+	return y
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+
+	return x
+}
