e2bc1fa574d9f700d4b8f8f281d39452b8bae319
showa-93 <shumai.1215.2018@gmail.com>
2022-11-05 22:30:08 +0900

    atcoder abc276 c

---
+++ contests/atcoder/abc276/c/main.go
@@ -0,0 +1,204 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"strconv"
+)
+
+const BufferSize int = 1e9
+
+const (
+	MinInt = -1 << (64 - 1)
+	MaxInt = 1<<(64-1) - 1
+)
+
+func main() {
+	solve(os.Stdin, os.Stdout)
+}
+
+func solve(in io.Reader, out io.Writer) {
+	r := NewReader(in)
+	w := NewWriter(out)
+	defer w.Flush()
+	n := r.ReadInt()
+	p := make([]int, n)
+	for i := 0; i < n; i++ {
+		p[i] = r.ReadInt()
+	}
+
+	if n == 2 {
+		p[0], p[1] = p[1], p[0]
+	} else {
+		PrevPermutation(p)
+	}
+
+	for i := 0; i < n; i++ {
+		w.w.Write([]byte(strconv.Itoa(p[i])))
+		w.w.WriteString(" ")
+	}
+
+}
+
+type reader struct {
+	s *bufio.Scanner
+}
+
+func NewReader(r io.Reader) *reader {
+	s := bufio.NewScanner(r)
+	s.Buffer(make([]byte, BufferSize), BufferSize)
+	s.Split(bufio.ScanWords)
+	return &reader{
+		s: s,
+	}
+}
+
+func (r *reader) Read() string {
+	r.s.Scan()
+	return r.s.Text()
+}
+
+func (r *reader) ReadInt() int {
+	r.s.Scan()
+	num, _ := strconv.Atoi(r.s.Text())
+
+	return num
+}
+
+func (r *reader) ReadLine(n int) []string {
+	line := make([]string, n)
+	for i := 0; i < n; i++ {
+		line[i] = r.Read()
+	}
+	return line
+}
+
+func (r *reader) ReadIntLine(n int) []int {
+	line := make([]int, n)
+	for i := 0; i < n; i++ {
+		line[i] = r.ReadInt()
+	}
+	return line
+}
+
+type writer struct {
+	w *bufio.Writer
+}
+
+func NewWriter(w io.Writer) *writer {
+	return &writer{
+		w: bufio.NewWriter(w),
+	}
+}
+
+func (w *writer) Flush() error {
+	return w.w.Flush()
+}
+
+func (w *writer) WriteString(s string) {
+	w.w.WriteString(s)
+	w.w.WriteRune('\n')
+}
+
+func (w *writer) WriteInt(v int) {
+	w.w.WriteString(strconv.Itoa(v))
+	w.w.WriteRune('\n')
+}
+
+func new1dInt(n, v int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = v
+	}
+
+	return a
+}
+
+func reset1dInt(a []int, v int) {
+	for i := 0; i < len(a); i++ {
+		a[i] = v
+	}
+}
+
+func new2dInt(m, n, v int) [][]int {
+	table := make([][]int, m)
+	for i := 0; i < m; i++ {
+		table[i] = make([]int, n)
+		for j := 0; j < n; j++ {
+			table[i][j] = v
+		}
+	}
+
+	return table
+}
+
+func reset2dInt(table [][]int, v int) {
+	for i := 0; i < len(table); i++ {
+		for j := 0; j < len(table[i]); j++ {
+			table[i][j] = v
+		}
+	}
+}
+
+func copy2dInt(dst, src [][]int) {
+	for i := 0; i < len(src); i++ {
+		copy(dst[i], src[i])
+	}
+}
+
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
+}
+
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+
+	return y
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+
+	return x
+}
+
+func PrevPermutation(list []int) bool {
+	if len(list) <= 2 {
+		return false
+	}
+
+	var i int
+	for i = len(list) - 2; i >= 0; i-- {
+		if list[i] >= list[i+1] {
+			break
+		}
+	}
+
+	if i < 0 {
+		return false
+	}
+
+	var j int
+	for j = len(list) - 1; j >= i; j-- {
+		if list[i] >= list[j] {
+			break
+		}
+	}
+
+	list[i], list[j] = list[j], list[i]
+
+	for p, q := i+1, len(list)-1; p < q; p, q = p+1, q-1 {
+		list[p], list[q] = list[q], list[p]
+	}
+
+	return true
+}
