e72f66eee80262f9bac9979dba93060728b9a222
showa-93 <shumai.1215.2018@gmail.com>
2023-04-15 21:54:12 +0900

    atcoder abc298 abc298_c

---
+++ contests/atcoder/abc298/c/main.go
@@ -0,0 +1,228 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+)
+
+const BufferSize int = 1e9
+
+const (
+	MinInt = -1 << (64 - 1)
+	MaxInt = 1<<(64-1) - 1
+)
+
+func main() {
+	solve(os.Stdin, os.Stdout)
+}
+
+func solve(in io.Reader, out io.Writer) {
+	reader := NewReader(in)
+	writer := NewWriter(out)
+	defer writer.Flush()
+	_, q := reader.ReadInt(), reader.ReadInt()
+
+	type Box struct {
+		Cards []int
+		Check map[int]struct{}
+	}
+	boxes := make(map[int]*Box)
+	cards := make(map[int]*Box)
+	for i := 0; i < q; i++ {
+		query := reader.ReadInt()
+		switch query {
+		case 1:
+			i, j := reader.ReadInt(), reader.ReadInt()
+			_, ok := boxes[j]
+			if !ok {
+				boxes[j] = &Box{
+					Cards: make([]int, 0),
+				}
+			}
+			boxes[j].Cards = append(boxes[j].Cards, i)
+
+			if _, ok := cards[i]; !ok {
+				cards[i] = &Box{
+					Cards: make([]int, 0),
+					Check: make(map[int]struct{}, 0),
+				}
+			}
+			if _, ok := cards[i].Check[j]; !ok {
+				cards[i].Check[j] = struct{}{}
+				cards[i].Cards = append(cards[i].Cards, j)
+			}
+		case 2:
+			i := reader.ReadInt()
+			sort.Slice(boxes[i].Cards, func(ii, jj int) bool {
+				return boxes[i].Cards[ii] < boxes[i].Cards[jj]
+			})
+			for _, c := range boxes[i].Cards {
+				writer.Int(c)
+			}
+			writer.Cr()
+		case 3:
+			i := reader.ReadInt()
+			sort.Slice(cards[i].Cards, func(ii, jj int) bool {
+				return cards[i].Cards[ii] < cards[i].Cards[jj]
+			})
+			for _, c := range cards[i].Cards {
+				writer.Int(c)
+			}
+			writer.Cr()
+		}
+	}
+}
+
+type reader struct {
+	s *bufio.Scanner
+}
+
+func NewReader(r io.Reader) *reader {
+	s := bufio.NewScanner(r)
+	s.Buffer(make([]byte, BufferSize), BufferSize)
+	s.Split(bufio.ScanWords)
+	return &reader{
+		s: s,
+	}
+}
+
+func (r *reader) Read() string {
+	r.s.Scan()
+	return r.s.Text()
+}
+
+func (r *reader) ReadInt() int {
+	r.s.Scan()
+	num, _ := strconv.Atoi(r.s.Text())
+
+	return num
+}
+
+func (r *reader) ReadLine(n int) []string {
+	line := make([]string, n)
+	for i := 0; i < n; i++ {
+		line[i] = r.Read()
+	}
+	return line
+}
+
+func (r *reader) ReadIntLine(n int) []int {
+	line := make([]int, n)
+	for i := 0; i < n; i++ {
+		line[i] = r.ReadInt()
+	}
+	return line
+}
+
+type writer struct {
+	w *bufio.Writer
+}
+
+func NewWriter(w io.Writer) *writer {
+	return &writer{
+		w: bufio.NewWriter(w),
+	}
+}
+
+func (w *writer) Flush() error {
+	return w.w.Flush()
+}
+
+func (w *writer) String(s string) {
+	w.w.WriteString(s)
+	w.Space()
+}
+
+func (w *writer) Int(v int) {
+	w.w.WriteString(strconv.Itoa(v))
+	w.Space()
+}
+
+func (w *writer) Space() {
+	w.w.WriteString(" ")
+}
+
+func (w *writer) Cr() {
+	w.w.WriteRune('\n')
+}
+
+func new1dInt(n, v int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = v
+	}
+
+	return a
+}
+
+func reset1dInt(a []int, v int) {
+	for i := 0; i < len(a); i++ {
+		a[i] = v
+	}
+}
+
+func new2dInt(m, n, v int) [][]int {
+	table := make([][]int, m)
+	for i := 0; i < m; i++ {
+		table[i] = make([]int, n)
+		for j := 0; j < n; j++ {
+			table[i][j] = v
+		}
+	}
+
+	return table
+}
+
+func reset2dInt(table [][]int, v int) {
+	for i := 0; i < len(table); i++ {
+		for j := 0; j < len(table[i]); j++ {
+			table[i][j] = v
+		}
+	}
+}
+
+func copy2dInt(dst, src [][]int) {
+	for i := 0; i < len(src); i++ {
+		copy(dst[i], src[i])
+	}
+}
+
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
+}
+
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+
+	return y
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+
+	return x
+}
+
+func Pow(a, b int) int {
+	p := 1
+	for b > 0 {
+		if b&1 == 1 {
+			p *= a
+		}
+		a *= a
+		b >>= 1
+	}
+
+	return p
+}
