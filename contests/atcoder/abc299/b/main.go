c4761ece4bc8830be53ac1519214554280046b7e
showa-93 <shumai.1215.2018@gmail.com>
2023-04-22 22:47:08 +0900

    atcoder abc299 abc299_b

---
+++ contests/atcoder/abc299/b/main.go
@@ -0,0 +1,219 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"strconv"
+)
+
+const BufferSize int = 1e9
+
+const (
+	MinInt = -1 << (64 - 1)
+	MaxInt = 1<<(64-1) - 1
+)
+
+func main() {
+	solve(os.Stdin, os.Stdout)
+}
+
+func solve(in io.Reader, out io.Writer) {
+	reader := NewReader(in)
+	writer := NewWriter(out)
+	defer writer.Flush()
+	n, t := reader.Int(), reader.Int()
+	c := reader.IntList(n)
+	r := reader.IntList(n)
+
+	ans1, cc := 0, 0
+	ans2, c0 := 0, 0
+	for i, ci := range c {
+		if ci == t && cc < r[i] {
+			ans1 = i
+			cc = r[i]
+		}
+		if ci == c[0] && c0 < r[i] {
+			ans2 = i
+			c0 = r[i]
+		}
+	}
+
+	if ans1 > 0 {
+		writer.Int(ans1 + 1)
+	} else {
+		writer.Int(ans2 + 1)
+	}
+}
+
+type reader struct {
+	s *bufio.Scanner
+}
+
+func NewReader(r io.Reader) *reader {
+	s := bufio.NewScanner(r)
+	s.Buffer(make([]byte, BufferSize), BufferSize)
+	s.Split(bufio.ScanWords)
+	return &reader{
+		s: s,
+	}
+}
+
+func (r *reader) String() string {
+	r.s.Scan()
+	return r.s.Text()
+}
+
+func (r *reader) Int() int {
+	r.s.Scan()
+	num, _ := strconv.Atoi(r.s.Text())
+
+	return num
+}
+
+func (r *reader) StringList(n int) []string {
+	line := make([]string, n)
+	for i := 0; i < n; i++ {
+		line[i] = r.String()
+	}
+	return line
+}
+
+func (r *reader) IntList(n int) []int {
+	line := make([]int, n)
+	for i := 0; i < n; i++ {
+		line[i] = r.Int()
+	}
+	return line
+}
+
+type writer struct {
+	w *bufio.Writer
+}
+
+func NewWriter(w io.Writer) *writer {
+	return &writer{
+		w: bufio.NewWriter(w),
+	}
+}
+
+func (w *writer) Flush() error {
+	return w.w.Flush()
+}
+
+func (w *writer) String(s string) {
+	w.w.WriteString(s)
+	w.Space()
+}
+
+func (w *writer) Int(v int) {
+	w.w.WriteString(strconv.Itoa(v))
+	w.Space()
+}
+
+func (w *writer) Space() {
+	w.w.WriteString(" ")
+}
+
+func (w *writer) Cr() {
+	w.w.WriteRune('\n')
+}
+
+func new1dInt(n, v int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = v
+	}
+
+	return a
+}
+
+func reset1dInt(a []int, v int) {
+	for i := 0; i < len(a); i++ {
+		a[i] = v
+	}
+}
+
+func new2dInt(m, n, v int) [][]int {
+	table := make([][]int, m)
+	for i := 0; i < m; i++ {
+		table[i] = make([]int, n)
+		for j := 0; j < n; j++ {
+			table[i][j] = v
+		}
+	}
+
+	return table
+}
+
+func reset2dInt(table [][]int, v int) {
+	for i := 0; i < len(table); i++ {
+		for j := 0; j < len(table[i]); j++ {
+			table[i][j] = v
+		}
+	}
+}
+
+func copy2dInt(dst, src [][]int) {
+	for i := 0; i < len(src); i++ {
+		copy(dst[i], src[i])
+	}
+}
+
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
+}
+
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+
+	return y
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+
+	return x
+}
+
+func Pow(a, b int) int {
+	p := 1
+	for b > 0 {
+		if b&1 == 1 {
+			p *= a
+		}
+		a *= a
+		b >>= 1
+	}
+
+	return p
+}
+
+func Factorial(a int) int {
+	v := 1
+	for i := 2; i <= a; i++ {
+		v *= i
+	}
+	return v
+}
+
+func Permutation(a, b int) int {
+	sum := 1
+	for i := a - b + 1; i <= a; i++ {
+		sum *= i
+	}
+
+	return sum
+}
+
+func Combination(a, b int) int {
+	return Permutation(a, b) / Factorial(b)
+}
