cc6d4da0c9d3a208d69b1b37fdb9badebc38e81e
showa-93 <shumai.1215.2018@gmail.com>
2023-04-29 09:07:28 +0900

    atcoder tessoku_book math_and_algorithm_am

---
+++ contests/atcoder/math_and_algorithm/am/main.go
@@ -0,0 +1,230 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"strconv"
+)
+
+const BufferSize int = 1e9
+
+const (
+	MinInt = -1 << (64 - 1)
+	MaxInt = 1<<(64-1) - 1
+)
+
+func main() {
+	solve(os.Stdin, os.Stdout)
+}
+
+func solve(in io.Reader, out io.Writer) {
+	reader := NewReader(in)
+	writer := NewWriter(out)
+	defer writer.Flush()
+	n, m := reader.Int(), reader.Int()
+	g := make([][]int, n)
+	for i := 0; i < m; i++ {
+		a, b := reader.Int()-1, reader.Int()-1
+		g[a] = append(g[a], b)
+		g[b] = append(g[b], a)
+	}
+
+	passed := make([]bool, n)
+	stack := make([]int, 0)
+	stack = append(stack, 0)
+	passed[0] = true
+	for len(stack) > 0 {
+		n := len(stack) - 1
+		u := stack[n]
+		stack = stack[:n]
+		for _, v := range g[u] {
+			if !passed[v] {
+				stack = append(stack, v)
+				passed[v] = true
+			}
+		}
+	}
+
+	for _, p := range passed {
+		if !p {
+			writer.String("The graph is not connected.")
+			return
+		}
+	}
+	writer.String("The graph is connected.")
+}
+
+type reader struct {
+	s *bufio.Scanner
+}
+
+func NewReader(r io.Reader) *reader {
+	s := bufio.NewScanner(r)
+	s.Buffer(make([]byte, BufferSize), BufferSize)
+	s.Split(bufio.ScanWords)
+	return &reader{
+		s: s,
+	}
+}
+
+func (r *reader) String() string {
+	r.s.Scan()
+	return r.s.Text()
+}
+
+func (r *reader) Int() int {
+	r.s.Scan()
+	num, _ := strconv.Atoi(r.s.Text())
+
+	return num
+}
+
+func (r *reader) StringList(n int) []string {
+	line := make([]string, n)
+	for i := 0; i < n; i++ {
+		line[i] = r.String()
+	}
+	return line
+}
+
+func (r *reader) IntList(n int) []int {
+	line := make([]int, n)
+	for i := 0; i < n; i++ {
+		line[i] = r.Int()
+	}
+	return line
+}
+
+type writer struct {
+	w *bufio.Writer
+}
+
+func NewWriter(w io.Writer) *writer {
+	return &writer{
+		w: bufio.NewWriter(w),
+	}
+}
+
+func (w *writer) Flush() error {
+	return w.w.Flush()
+}
+
+func (w *writer) String(s string) *writer {
+	w.w.WriteString(s)
+	return w
+}
+
+func (w *writer) Int(v int) *writer {
+	w.w.WriteString(strconv.Itoa(v))
+	return w
+}
+
+func (w *writer) Space() *writer {
+	w.w.WriteString(" ")
+	return w
+}
+
+func (w *writer) Cr() *writer {
+	w.w.WriteRune('\n')
+	return w
+}
+
+func new1dInt(n, v int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = v
+	}
+
+	return a
+}
+
+func reset1dInt(a []int, v int) {
+	for i := 0; i < len(a); i++ {
+		a[i] = v
+	}
+}
+
+func new2dInt(m, n, v int) [][]int {
+	table := make([][]int, m)
+	for i := 0; i < m; i++ {
+		table[i] = make([]int, n)
+		for j := 0; j < n; j++ {
+			table[i][j] = v
+		}
+	}
+
+	return table
+}
+
+func reset2dInt(table [][]int, v int) {
+	for i := 0; i < len(table); i++ {
+		for j := 0; j < len(table[i]); j++ {
+			table[i][j] = v
+		}
+	}
+}
+
+func copy2dInt(dst, src [][]int) {
+	for i := 0; i < len(src); i++ {
+		copy(dst[i], src[i])
+	}
+}
+
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
+}
+
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+
+	return y
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+
+	return x
+}
+
+func Pow(a, b int) int {
+	p := 1
+	for b > 0 {
+		if b&1 == 1 {
+			p *= a
+		}
+		a *= a
+		b >>= 1
+	}
+
+	return p
+}
+
+func Factorial(a int) int {
+	v := 1
+	for i := 2; i <= a; i++ {
+		v *= i
+	}
+	return v
+}
+
+func Permutation(a, b int) int {
+	sum := 1
+	for i := a - b + 1; i <= a; i++ {
+		sum *= i
+	}
+
+	return sum
+}
+
+func Combination(a, b int) int {
+	return Permutation(a, b) / Factorial(b)
+}
---
+++ contests/atcoder/math_and_algorithm/am/main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bytes"
+	"os"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestSolve_Case1(t *testing.T) {
+	in, err := os.Open("testdata/case1/in")
+	if err != nil {
+		t.Fatal(err)
+	}
+	buf := bytes.NewBuffer(make([]byte, 0))
+
+	solve(in, buf)
+
+	want, err := os.ReadFile("testdata/case1/out")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if diff := cmp.Diff(buf.String(), string(want)); diff != "" {
+		t.Error(diff)
+	}
+}
+
+func TestSolve_Case2(t *testing.T) {
+	in, err := os.Open("testdata/case2/in")
+	if err != nil {
+		t.Fatal(err)
+	}
+	buf := bytes.NewBuffer(make([]byte, 0))
+
+	solve(in, buf)
+
+	want, err := os.ReadFile("testdata/case2/out")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if diff := cmp.Diff(buf.String(), string(want)); diff != "" {
+		t.Error(diff)
+	}
+}
